df25f67d39e03b203bb5fad4eb64efaeef3d8cb3
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-05-08 23:13:44 +0200

    Deleting unnecessary objects
    Deleting unnecessary objects
    
    Co-Authored-By: Mateusz Paszko <73493587+nieswietymikolaj@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Serce gry
-const config = {
+let game = new Phaser.Game({
     type: Phaser.AUTO,
     width: 1200,
     height: 790,
@@ -11,30 +11,10 @@ const config = {
     physics: {
         default: 'arcade',
         arcade: {
-            //gravity: { y: 500 },
             debug: true
         }
     }
-}
-
-let game = new Phaser.Game(config)
-
-// let game = new Phaser.Game({
-//     type: Phaser.AUTO,
-//     width: 1200,
-//     height: 790,
-//     scene: {
-//         preload: preload,
-//         create: create,
-//         update: update
-//     },
-//     physics: {
-//         default: 'arcade',
-//         arcade: {
-//             debug: true
-//         }
-//     }
-// })
+})
 
 //Ładowanie zasobów
 function preload() {
@@ -75,12 +55,12 @@ function preload() {
     })
 }
 
-var coin
 //Tworzenie obiektów gry
 function create() {
     background = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'background').setOrigin(0)
     score = this.add.bitmapText(50, 10, 'Desyrel', '0', 35)
 
+    //console.log(game)
     //this.scene.pause()
     layers = []
     for (let i = 0; i < 10; i++) {
@@ -129,45 +109,20 @@ function create() {
         frameRate: 6, repeat: -1
     })
 
-    // redBullets = this.add.group()
-    // redBullets.createMultiple(16, "redBullet")
-    // redBullets.setAll('anchor.x', 0.5);
-    // redBullets.setAll('anchor.y', 1);
-    // redBullets.setAll('outOfBoundsKill', true);
-    // redBullets.setAll('checkWorldBounds', true);
-
-    // redBullet = redBullets.getFirstExists(false, true, 64, 64, "redBullet", 0)
-
     jump = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
-    player = this.physics.add.sprite(150, game.config.height / 2 - 50, 'flyingPlayer')
-    //player.setOrigin()
+    player = this.physics.add.sprite(150, (game.config.height / 2) - 50, 'flyingPlayer')
     player.body.gravity.y = 500
-    player.anims.play('flyingPlayer', true)
     player.setBodySize(80, 140, false).setOffset(65, 0)
-
-    // bullets.createMultiple(5, 'redBullet')
-    // bullets.setAll('anchor.y', 0.5)
-    // bullets.setAll('outOfBoundsKill', true)
-    // bullets.setAll('checkWorldBounds', true)
-
-    //enemy = this.physics.add.sprite(1500, Math.floor(Math.random() * (game.config.height - 50)) + 50, 'redBullet')
-    //enemy.setOrigin()
-    //enemy.setVelocityX(-300)
-    //enemy.setVelocityY(-200)
-    //enemy.anims.play('redBullet', true)
-    //enemy.body.bounce.set(1)
-    //enemy.setBodySize(40, 40, false).setOffset(0, 13)
+    player.anims.play('flyingPlayer', true)
 
     scoreCoin = this.add.sprite(30, 35, 'coin').setScale(1.5)
     scoreCoin.anims.play('coin', true)
 
-    coins = this.physics.add.group();
-    bullets = this.physics.add.group();
+    coins = this.physics.add.group()
+    bullets = this.physics.add.group()
 
-    //https://www.w3schools.com/js/js_random.asp
-    // coin2 = this.physics.add.sprite(1200, Math.floor(Math.random() * (game.config.height - 50)) + 50, 'coin').setScale(1.5)
-    // coin2.setVelocityX(-300)
-    // coin2.anims.play('coin', true)
+    this.time.addEvent({ delay: 3000, callback: addCoin, callbackScope: this, loop: true });
+    this.time.addEvent({ delay: 2000, callback: addBullet, callbackScope: this, loop: true });
 
     //this.cameras.main.setBounds(0, 0, game.config.width, game.config.height) //Kamera porusza się tylko w obrębie świata
     this.physics.world.setBounds(0, 0, game.config.width, game.config.height - 45) //Świat gry jest większy/mniejszy niż ekran
@@ -176,34 +131,27 @@ function create() {
 
     //Kolizja z krawędziami świata
     player.body.setCollideWorldBounds(true)
-    //enemy.body.setCollideWorldBounds(true)
+
+    //left = this.physics.world.bounds.left;
 
     //Inne ciała nie mogą go przesuwać
     player.body.immovable = true
-
-
-    this.time.addEvent({ delay: 4000, callback: addCoin, callbackScope: this, loop: true });
-
-    this.time.addEvent({ delay: 2000, callback: addBullet, callbackScope: this, loop: true });
 }
 
 function addCoin() {
     if (!death) {
-        coin = coins.create(1200, Math.floor(Math.random() * (game.config.height - 100) + 50), 'coin');
-        coin.setVelocityX(-250)
-        //bullet.setVelocityY(-300)
+        coin = coins.create(1500, Math.floor(Math.random() * (game.config.height - 100) + 50), 'coin').setScale(1.5)
+        coin.setVelocityX(-300)
         coin.anims.play('coin', true)
-        //coin.body.bounce.set(1)
-        //coin.body.setCollideWorldBounds(true)
     }
 }
 
 function addBullet() {
-    //type = 'blueBullet';
-    choice = Math.floor(Math.random() * 3) + 1;
+    choice = Math.floor(Math.random() * 4) + 1;
+
     if (choice == 1) {
         //type = 'redBullet';
-        bullet = bullets.create(1200, Math.floor(Math.random() * (game.config.height - 50) + 50), 'redBullet');
+        bullet = bullets.create(1500, Math.floor(Math.random() * (game.config.height - 100) + 50), 'redBullet');
         bullet.setBodySize(40, 40, false).setOffset(0, 13)
         bullet.setVelocityX(-450)
         bullet.anims.play('redBullet', true)
@@ -211,17 +159,17 @@ function addBullet() {
         bullet.body.setCollideWorldBounds(true)
     } else if (choice == 2) {
         //type = 'blueBullet';
-        bullet = bullets.create(1200, Math.floor(Math.random() * (game.config.height - 50) + 50), 'blueBullet');
+        bullet = bullets.create(1500, Math.floor(Math.random() * (game.config.height - 100) + 50), 'blueBullet');
         bullet.setBodySize(40, 40, false).setOffset(0, 13)
-        bullet.setVelocityX(-500)
+        bullet.setVelocityX(-550)
         bullet.anims.play('blueBullet', true)
         //bullet.body.bounce.set(1)
         //bullet.body.setCollideWorldBounds(true)
     } else if (choice == 3) {
         //type = 'pinkBullet';
-        bullet = bullets.create(1200, Math.floor(Math.random() * (game.config.height - 50) + 50), 'pinkBullet');
+        bullet = bullets.create(1500, Math.floor(Math.random() * (game.config.height - 100) + 50), 'pinkBullet');
         bullet.setBodySize(40, 40, false).setOffset(0, 13)
-        bullet.setVelocityX(-400)
+        bullet.setVelocityX(-500)
         direction = Math.floor(Math.random() * 2) + 1;
         if (direction == 1) {
             bullet.setVelocityY(250)
@@ -270,18 +218,32 @@ function update() {
         if (player.body.touching.down || player.body.onFloor()) player.anims.play('runningPlayer', true)
         else player.anims.play('flyingPlayer', true)
 
-        bullets.children.iterate((bullet) => {
+        coins.children.iterate((coin) => {
+            if (coin != undefined) {
+                if (coin.x <= -50) coins.remove(coin, true, true)
+                //console.log(coin.x)
+            }
+            //console.log(coins)
+        })
+    }
+
+    bullets.children.iterate((bullet) => {
+        if (bullet != undefined) {
             if (bullet.texture.key == "redBullet") {
                 if (player.y > bullet.y) bullet.y += 0.6
                 else bullet.y -= 0.6
-                //console.log(bullet)
+                //console.log(bullet.x)
+                //console.log(bullets)
             }
-        })
-    }
+
+            if (bullet.x < -150) bullets.remove(bullet, true, true)
+        }
+    })
 
     //Wywołanie funkcji przy kolizji
     this.physics.collide(player, bullets, collision)
     this.physics.collide(player, coins, collectCoin)
+    //this.physics.collide(left, bullets, collision2)
 }
 
 function collision(player, bullets) {
@@ -295,3 +257,8 @@ function collectCoin(player, coins) {
     coins.destroy()
     score.text = (parseInt(score.text) + 1).toString()
 }
+
+// function collision2(left, bullets) {
+//     bullets.destroy()
+//     console.log("XD")
+// }
